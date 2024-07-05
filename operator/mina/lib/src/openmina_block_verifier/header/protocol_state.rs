54e947d6619111d985ae8cc2015946e92fd7abf3
gabrielbosio <gabrielbosio95@gmail.com>
2024-07-05 13:20:07 -0300

    Remove test that only covers external deps

--- operator/mina/lib/src/openmina_block_verifier/header/protocol_state.rs
+++ operator/mina/lib/src/openmina_block_verifier/header/protocol_state.rs
@@ -30,28 +30,3 @@ impl Into<MinaStateProtocolStateValueStableV2> for ProtocolState {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use std::str::FromStr as _;
-
-    use mina_p2p_messages::v2::StateHash;
-
-    use crate::openmina_block_verifier::header::protocol_state_query::ProtocolStateQuery;
-
-    const MINA_PROTOCOL_STATE_QUERY: &str = include_str!(
-        "../../../../../../batcher/aligned/test_files/mina/mina_protocol_state_query.json"
-    );
-
-    #[test]
-    fn test_protocol_state_parse() {
-        let protocol_state_query: ProtocolStateQuery =
-            serde_json::from_str(MINA_PROTOCOL_STATE_QUERY).unwrap();
-        let protocol_state = &protocol_state_query.data.best_chain[0].protocol_state;
-        let previous_state_hash = StateHash::from_str(&protocol_state.previous_state_hash).unwrap();
-        assert_eq!(
-            previous_state_hash.to_string(),
-            protocol_state.previous_state_hash
-        );
-    }
-}
